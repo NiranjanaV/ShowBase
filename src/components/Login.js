52c071626967f21770516682db408907d28f06fb
swaathivelavan <71623028+swaathivelavan@users.noreply.github.com>
2022-02-27 01:21:48 -0500

    login design finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './login.css';
 
+
 function Login(){
     return(
 
@@ -10,17 +11,19 @@ function Login(){
 <h2>Sign in</h2>
   <div class="input-container">
     <input required type="text" />
-    <label>Name</label>
+    <label>Email</label>
   </div>
   
   <div class="input-container">
     <input required type="text" />
-    <label>Email</label>
+    <label>Password</label>
   </div>
   <div class="input-container">
     <input type="submit" value="Submit" />
   </div>
+  <h4>New to ShowBase?<span> Sign up now.</span></h4>
 </form>
+
 </div>
 
     )
