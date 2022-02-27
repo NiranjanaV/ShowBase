1c86cab6c42bb48305ca0e287c6cc05be9a4eff0
swaathivelavan <71623028+swaathivelavan@users.noreply.github.com>
2022-02-26 22:20:38 -0500

    login page ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './login.css';
 
 function Login(){
     return(
 
-    <h2>Login Page</h2>
+        <div>
+
+<form>
+<h2>Sign in</h2>
+  <div class="input-container">
+    <input required type="text" />
+    <label>Name</label>
+  </div>
+  
+  <div class="input-container">
+    <input required type="text" />
+    <label>Email</label>
+  </div>
+  <div class="input-container">
+    <input type="submit" value="Submit" />
+  </div>
+</form>
+</div>
+
     )
 }
 
