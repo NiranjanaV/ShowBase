53ce40759aa430b638d076a5d432ae6e96a687e3
Srikant33 <96463545+Srikant33@users.noreply.github.com>
2022-03-04 18:46:54 -0500

    pull commit

--- backend/functions/dbAccess/Sample/database.go
+++ backend/functions/dbAccess/Sample/database.go
@@ -4,8 +4,6 @@ import (
 	// M "/GoCode/Create_Table.go"
 	// N "/GoCode/controllers"
 	// L "ShowBase/data"
-	"database/sql"
-	"fmt"
 
 	// "log"
 	// "os"
@@ -13,25 +11,25 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func dab() {
-	// var database *sql.DB
-	// os.Remove("user_data-database.db")                               // I delete the file to avoid duplicated records.
-	sqliteDatabase, _ := sql.Open("sqlite3", "./../dbSQLite/user_data-database") // Open the created SQLite File
-	defer sqliteDatabase.Close()
-	// database = sqliteDatabase
-	var tablename = "user_auth"
+// func dab() {
+// 	// var database *sql.DB
+// 	// os.Remove("user_data-database.db")                               // I delete the file to avoid duplicated records.
+// 	sqliteDatabase, _ := sql.Open("sqlite3", "./../dbSQLite/user_data-database") // Open the created SQLite File
+// 	defer sqliteDatabase.Close()
+// 	// database = sqliteDatabase
+// 	var tablename = "user_auth"
 
-	// fmt.Println("Create db")
-	// O.CreateDB()
+// 	// fmt.Println("Create db")
+// 	// O.CreateDB()
 
-	fmt.Println("Create table")
-	CreateTable(sqliteDatabase, tablename) // Create Database Tables
+// 	fmt.Println("Create table")
+// 	CreateTable(sqliteDatabase, tablename) // Create Database Tables
 
-	// INSERT RECORDS
-	fmt.Println("insert")
-	InsertStudent(sqliteDatabase, tablename, "1", "username", "password")
+// 	// INSERT RECORDS
+// 	fmt.Println("insert")
+// 	InsertStudent(sqliteDatabase, tablename, "1", "username", "password")
 
-	// DISPLAY INSERTED RECORDS
-	fmt.Println("display")
-	DisplayStudents(sqliteDatabase, tablename)
-}
+// 	// DISPLAY INSERTED RECORDS
+// 	fmt.Println("display")
+// 	DisplayStudents(sqliteDatabase, tablename)
+// }
