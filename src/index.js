eafea326ef5bbaea5676b5235b17ae653ad38265
swaathivelavan <71623028+swaathivelavan@users.noreply.github.com>
2022-04-01 17:26:08 -0400

    #77 axios call for authenticateUser

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 
 import {BrowserRouter} from 'react-router-dom'
 
+import { AuthProvider } from './context/AuthProvider';
+
 
   
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
